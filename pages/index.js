db6967889f5dbc616691cd9e98e61980e01a6d96
Evgeny Klimenchenko <Zklimenchenko@gmail.com>
2020-05-20 23:17:46 +0100

    Blog connected

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import GhostContentAPI from "@tryghost/content-api";
+
+// Create API instance with site credentials
+const api = new GhostContentAPI({
+    url: 'https://ghost.evgeny.dev',
+    key: 'f84895b09f5b555c1661372509',
+    version: "v3"
+});
+
+export async function getPosts() {
+    const result = await api.posts
+        .browse({
+            limit: "all"
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    console.log(result)
+}
+
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
-export default () => <Title>My page</Title>
+export default () => {
+    getPosts()
+    return (<Title>My page</Title>)
+}
