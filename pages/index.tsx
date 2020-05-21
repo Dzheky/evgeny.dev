f781853f356734561d3852a619a4f987f52b5746
Evgeny Klimenchenko <Zklimenchenko@gmail.com>
2020-05-21 22:35:26 +0100

    added eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import GhostContentAPI from "@tryghost/content-api";
+import GhostContentAPI from '@tryghost/content-api'
 
 // Create API instance with site credentials
 const api = new GhostContentAPI({
     url: 'https://ghost.evgeny.dev',
     key: 'f84895b09f5b555c1661372509',
-    version: "v3"
-});
+    version: 'v3',
+})
 
 export async function getPosts() {
     const result = await api.posts
         .browse({
-            limit: "all"
+            limit: 'all',
         })
-        .catch(err => {
-            console.error(err);
-        });
-    console.log(result)
+        .catch((err) => {
+            console.error(err)
+        })
 }
 
 const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+    font-size: 50px;
+    color: ${({ theme }) => theme.colors.primary};
 `
 
 export default () => {
     getPosts()
-    return (<Title>My page</Title>)
+    return <Title>My page</Title>
 }
