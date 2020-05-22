5cc5c6ed389f07d035dae4b04a9163caa76f7efc
Evgeny Klimenchenko <Zklimenchenko@gmail.com>
2020-05-22 22:09:09 +0100

    changed to tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,27 +5,27 @@ import GhostContentAPI from '@tryghost/content-api'
 
 // Create API instance with site credentials
 const api = new GhostContentAPI({
-    url: 'https://ghost.evgeny.dev',
-    key: 'f84895b09f5b555c1661372509',
-    version: 'v3',
+  url: 'https://ghost.evgeny.dev',
+  key: 'f84895b09f5b555c1661372509',
+  version: 'v3',
 })
 
 export async function getPosts() {
-    const result = await api.posts
-        .browse({
-            limit: 'all',
-        })
-        .catch((err) => {
-            console.error(err)
-        })
+  const result = await api.posts
+    .browse({
+      limit: 'all',
+    })
+    .catch((err) => {
+      console.error(err)
+    })
 }
 
 const Title = styled.h1`
-    font-size: 50px;
-    color: ${({ theme }) => theme.colors.primary};
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
 `
 
 export default () => {
-    getPosts()
-    return <Title>My page</Title>
+  getPosts()
+  return <Title>My page</Title>
 }
