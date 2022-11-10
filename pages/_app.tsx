092ab03726fc16666579fd6ecb6a37949899cfd4
Evgeny Klimenchenko <Zklimenchenko@gmail.com>
2022-11-10 09:33:53 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -89,8 +89,6 @@ const App = ({ Component, pageProps }: AppProps) => {
   const body = (
     <ThemeProvider theme={chosenTheme}>
       <Head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8335745014170543"
-     crossOrigin="anonymous"></script>
         <title>Blog posts</title>
       </Head>
       <DefaultSeo {...SEO} />
